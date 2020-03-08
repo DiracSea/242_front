dcf0814cab994058f370ad1cb5714905b45fa6dd
Longze SU <lsu018@ucr.edu>
2020-03-07 17:39:06 -0800

    2nd deploy

---
+++ js/hint.js
@@ -0,0 +1,19 @@
+var availableTags1;
+
+// $(function() {
+//     var availableTags = ["California"];
+//      $( "#tags" ).autocomplete({
+//          source:availableTags
+//         })
+//  });
+
+$.get("file/act_hint.txt", function(data) {
+    availableTags1 = data.split('\n');
+     $( "#acts" ).autocomplete({
+         source:availableTags1
+        })
+ });
+
+$(document).ready(function() {
+    $('#dtBasicExample').DataTable();
+} );
