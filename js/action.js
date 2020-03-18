bce0ea194f99f61628b58e00fced6b52d5554244
Longze SU <lsu018@ucr.edu>
2020-03-17 21:56:00 -0700

    final

--- js/action.js
+++ js/action.js
@@ -1,18 +1,52 @@
 // var row = document.getElementById('#tbl').insertRow();
-
+var res1;
+var res2;
+var res3;
+var res; 
+$.get("file/rankingResultTweet.txt", function(data) {
+    res1 = data.split('\n');
+});
+$.get("file/rankingResultUser.txt", function(data) {
+    res2 = data.split('\n');
+});
+$.get("file/rankResultSimilarity.txt", function(data) {
+    res3 = data.split('\n');
+});
 
 $(document).ready(function(){
 	
 	$("#btn").click(function(){
-        alert($("#rank").val());
+        var r = $("#rank").val();
+        var s = $("#acts").val();
+        var k = 3; 
         $("#tbl tr").remove(); 
-        var row = $("<tr>");
 
-        row.append($("<td>Text-1</td>"))
-           .append($("<td>Text-2</td>"))
-           .append($("<td>Text-3</td>"));
+        // var str = document.app.search(r,s,k);
+
+
+        // var res = str.split(",")
+        var row1 = $("<tr>");
+        var row2 = $("<tr>");
+        var row3 = $("<tr>");
+
+        if (r == "Rank By Tweet" && s == "food") res = res1; 
+        else if (r == "Rank By User" && s == "entertainment") res = res2; 
+        else if (r == "Rank By Similarity" && s == "art") res = res3; 
+        else alert("Input Out of Sample"); 
+
+        row1.append($("<td>"+r+"</td>"))
+           .append($("<td>"+res[0].split(",")[0]+"</td>"))
+           .append($("<td>"+res[0].split(",")[1]+"</td>"));
+        row2.append($("<td>"+r+"</td>"))
+           .append($("<td>"+res[1].split(",")[0]+"</td>"))
+           .append($("<td>"+res[1].split(",")[1]+"</td>"));
+        row3.append($("<td>"+r+"</td>"))
+           .append($("<td>"+res[2].split(",")[0]+"</td>"))
+           .append($("<td>"+res[2].split(",")[1]+"</td>"));
        
-        $("#tbl tbody").append(row);
+        $("#tbl tbody").append(row1);
+        $("#tbl tbody").append(row2);
+        $("#tbl tbody").append(row3);
 		
 	});
 });
---
+++ readme.md
@@ -0,0 +1,14 @@
+# WebPage Instruction
+
+### use chrome with parameter in command line:
+```
+> chrome --allow-file-access-from-files
+```
+### Although there are hints in the search box, we provide three type of ranking function with samples of search 
+
+- Rank By Tweet
+  - food
+- Rank By User
+  - entertainment
+- Rank By Similarity
+  - art
